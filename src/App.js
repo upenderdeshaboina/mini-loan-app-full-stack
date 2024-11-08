3c735191e0a41396a3fec7357c499af553ec7c53
upenderdeshaboina <upenderdeshaboina12@gmail.com>
2024-11-08 18:28:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch,} from 'react-router-dom';
+import Dashboard from './Dashboard/Dashboard';
+import Login from './Auth/Login';
+import Register from './Auth/Register';
+import RepaymentList from './Repayment/RepaymentList'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    
+    return (
+        <div className='app-container'>
+            <Router>
+                <Switch>
+                    <Route exact path="/Dashboard" component={Dashboard} />
+                    <Route exact path="/" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path='/payments' component={RepaymentList}/>
+                </Switch>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
